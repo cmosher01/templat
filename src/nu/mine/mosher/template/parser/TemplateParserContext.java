ebfa7ba207e2865848904fc7fa03393f8b33ad18
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2006-10-22 22:25:34 +0000

    remove "eclipsing" concept; make getValue throw if variable is not found
    git-svn-id: http://svn.assembla.com/svn/cmosher/templat@8442 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- src/nu/mine/mosher/template/parser/TemplateParserContext.java
+++ src/nu/mine/mosher/template/parser/TemplateParserContext.java
@@ -5,34 +5,51 @@ package nu.mine.mosher.template.parser;
 
 import java.util.HashMap;
 import java.util.Map;
+import nu.mine.mosher.template.exception.TemplateParsingException;
 
-public class TemplateParserContext implements Cloneable
+/**
+ * Represents a context of variables. Basically, a context is simply
+ * a mapping of variable names to values.
+ *
+ * @author Chris Mosher
+ */
+public class TemplateParserContext
 {
 	private final Map<String,Object> variables = new HashMap<String,Object>();
-	private boolean eclipsing;
 
+	/**
+	 * @param varName
+	 * @param varValue
+	 */
 	public void addVariable(final String varName, final Object varValue)
 	{
 		this.variables.put(varName,varValue);
 	}
 
+	/**
+	 * Checks to see if the given variable is defined in
+	 * this context.
+	 * @param varName name of variable to check
+	 * @return <code>true</code> if the given variable is defined
+	 */
 	public boolean isDefined(final String varName)
 	{
 		return this.variables.containsKey(varName);
 	}
 
-	public Object getValue(final String varName)
+	/**
+	 * Gets the value of a given variable in this context.
+	 * @param varName name of variable to get
+	 * @return the value of the given variable
+	 * @throws TemplateParsingException if <code>varName</code>
+	 * is not defined in this context
+	 */
+	public Object getValue(final String varName) throws TemplateParsingException
 	{
+		if (!isDefined(varName))
+		{
+			throw new TemplateParsingException("Cannot find "+varName);
+		}
 		return this.variables.get(varName);
-	}
-
-	public void setEclipsing(final boolean eclipsing)
-	{
-		this.eclipsing = eclipsing;
-	}
-
-	public boolean isEclipsing()
-	{
-		return this.eclipsing;
 	}
 }
