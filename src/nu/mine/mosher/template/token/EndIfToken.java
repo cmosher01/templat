7cf037e1833c999fc214f13607ccdb83341a896c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2006-10-22 22:29:06 +0000

    make sure "end if" comes within an "if" block
    git-svn-id: http://svn.assembla.com/svn/cmosher/templat@8444 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- src/nu/mine/mosher/template/token/EndIfToken.java
+++ src/nu/mine/mosher/template/token/EndIfToken.java
@@ -3,6 +3,7 @@
  */
 package nu.mine.mosher.template.token;
 
+import nu.mine.mosher.template.exception.TemplateParsingException;
 import nu.mine.mosher.template.parser.TemplateParser;
 
 class EndIfToken extends TemplateToken
@@ -14,8 +15,9 @@ class EndIfToken extends TemplateToken
 	}
 
 	@Override
-	public void parse(final TemplateParser parser, final StringBuilder appendTo)
+	public void parse(final TemplateParser parser, final StringBuilder appendTo) throws TemplateParsingException
 	{
+		parser.getContext().current().getValue(TemplateParser.VAR_IF); // make sure we're in an if-block
 		parser.getContext().pop();
 	}
 }
